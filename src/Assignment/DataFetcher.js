ccbdc50520d0598783122b150815ca21b124de73
sunil0336 <sunilrathod.0336@gmail.com>
2025-02-11 17:00:53 +0530

    update e3

--- src/Assignment/DataFetcher.js
+++ src/Assignment/DataFetcher.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class DataFetcher extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             data: []
         }
     }
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => response.json())
-            .then(json => this.setState({data: json}))
-            .catch((error) => console.error("Error fetching data:", error));
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/todos?_limit=4')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             console.log(json);
+    //             this.setState({data: json})})
+    //         .catch((error) => console.error("Error fetching data:", error));
 
+    // }
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/todos?_limit=4")
+            .then((response) => {
+                console.log("Fetched Data:", response.data);
+                this.setState({ data: response.data });
+            })
+            .catch((error) => console.error("Error fetching data:", error));
     }
-    
+
     render() {
         return (
             <>
-            <h1>DataFetcher</h1>
-            <p>Id: {this.state.data.id}</p>
-            <p>Title: {this.state.data.title}</p>
-            <p>completed: {this.state.data.completed ? 'Completed' : 'Not Completed'}</p>
+                <h1>DataFetcher</h1>
+                <ul>
+                    {this.state.data.map((todo) => (
+                        <li key={todo.id} style={{ marginBottom: "10px" }}>
+                            <b>ID:</b> {todo.id} <br /><b>Title:</b> {todo.title}<br />
+                            <b> Completed:</b> {this.state.data.completed ? 'Completed' : 'Not Completed'}
+                        </li>
+                    ))}
+                </ul>
             </>
         );
     }
@@ -36,3 +53,5 @@ export default DataFetcher
 //     "title": "delectus aut autem",
 //     "completed": false
 //   }
+
+// npm install axios
